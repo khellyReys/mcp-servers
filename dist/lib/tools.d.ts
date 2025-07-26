dcdac793aa7174f4f2f0f51a331424b11cc95bbf
khellyReys <reyesangelo101292@gmail.com>
2025-07-26 09:12:33 +0800

    Added types.d.ts

---
+++ dist/lib/tools.d.ts
@@ -0,0 +1,6 @@
+/**
+ * Discovers and loads available tools from the tools directory
+ * @returns {Promise<Array>} Array of tool objects
+ */
+export declare function discoverTools(): Promise<any[]>;
+//# sourceMappingURL=tools.d.ts.map
